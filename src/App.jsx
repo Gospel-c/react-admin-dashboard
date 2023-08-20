c7d265852a20e0adf0e158b6f84a5a0dee1bf2c7
Gospel Amanze <ollygos@gmail.com>
2023-08-20 01:33:04 +0100

    commit of themes and boilerplates

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { ColorModeContext, useMode } from './themes'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+   <div className='app'></div>
+  )
+}
+
+export default App
